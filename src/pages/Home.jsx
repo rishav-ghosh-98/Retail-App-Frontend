df2b6a0fc20257b43d21aaed45a853fd47dd4e47
Rishav Ghosh <rishavarighs@gmail.com>
2026-02-20 02:31:23 +0530

    Added the categories images in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,35 @@
-import Nav from "./Nav"
+import Nav from "./Nav";
+import MenImage from "../assets/MenImage.png"
+import WomenImage from "../assets/WomenImage.png"
+import Kids from "../assets/Kids.png"
+import Electronics from "../assets/Electronics.png"
 const Home = () => {
-    return (
-        <>
+  return (
+    <>
+      <div>
+        <Nav />
+        <br />
         <div>
-            <Nav />
-            <h1>Home</h1>
+        <div className="container">
+            <h3>Categories</h3>
+          <div class="row">
+            <div class="col-3">
+                <img src={MenImage} alt="Men" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div class="col-3">
+                <img src={WomenImage} alt="Men" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div class="col-3">
+                <img src={Kids} alt="Men" style={{ width: "100%", height: "auto" }} />
+            </div>
+            <div class="col-3">
+                <img src={Electronics} alt="Men" style={{ width: "100%", height: "auto" }} />
+            </div>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+      </div>
+    </>
+  );
+};
 export default Home;
