a5653028459624bad1a894a9b5a4cfe66d446cf8
Rishav Ghosh <rishavarighs@gmail.com>
2026-02-20 02:54:54 +0530

    Added the categories and did the routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Categories from './pages/Categories.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/categories",
+    element: <Categories />,
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
