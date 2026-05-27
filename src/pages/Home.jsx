6326e8a9d065bc374cec5f15c3fdfea22a15c81b
rishav-ghosh-98 <rishavghosh0798@gmail.com>
2026-05-27 15:49:08 +0530

    changes for category images in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import Loader from "../components/Loader";
 import { ENDPOINTS } from "../api/endpoints";
 import useFetch from "../hooks/useFetch";
 const Home = () => {
-  const { data : response , loading, error} = useFetch(ENDPOINTS.categories)
+  const { data: response, loading, error } = useFetch(ENDPOINTS.categories);
   const categories = response?.data?.categories || [];
   return (
     <>
@@ -17,90 +17,63 @@ const Home = () => {
         <br />
         <div>
           <div className="container">
-            <h3>Categories</h3>
+            <div className="md-4">
+               <h3>Categories</h3>
+            </div>
+           
+
             {loading && <Loader />}
             {error && <p>Error loading categories:{error}</p>}
-            {/* <div class="row">
-              <div class="col-3" style={{ flex: "0 0 20%" }}>
-                <NavLink to="/categories" className="text-decoration-none">
-                  <img
-                    src={MenImage}
-                    alt="Men"
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </NavLink>
-              </div>
-              <div class="col-3" style={{ flex: "0 0 20%" }}>
-                <NavLink to="/categories" className="text-decoration-none">
-                  <img
-                    src={WomenImage}
-                    alt="Women"
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </NavLink>
-              </div>
-              <div class="col-3" style={{ flex: "0 0 20%" }}>
-                <NavLink to="/categories" className="text-decoration-none">
-                  <img
-                    src={Kids}
-                    alt="Kids"
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </NavLink>
-              </div>
-              <div class="col-3" style={{ flex: "0 0 20%" }}>
-                <NavLink to="/categories" className="text-decoration-none">
-                 <img
-                  src={Electronics}
-                  alt="Electronics"
-                  style={{ width: "100%", height: "auto" }}
-                /></NavLink>
-              </div>
-              <div class="col-3" style={{ flex: "0 0 20%" }}>
-                <NavLink to="/categories" className="text-decoration-none">
-                  <img
-                    src={Beauty}
-                    alt="Beauty"
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </NavLink>
-              </div>
-            </div> */}
             <div className="row">
-              {categories.map((cat) => (<div key ={cat._id}className="col-3" style={{ flex: "0 0 20%" }}>
-                <NavLink to={`/categories/${cat._id}`} className="text-decoration-none">
-                <div style={{ position: "relative" }}>
-                    <img
-                      src={`${import.meta.env.VITE_API_URL}/images/${cat.image}`}
-                      alt={cat.name}
-                      style={{ width: "100%", height: "auto" }}
-                    />
-                    <div
-                      style={{
-                        position: "absolute",
-                        bottom: 0,
-                        width: "100%",
-                        background: "rgba(255,255,255,0.8)",
-                        textAlign: "center",
-                        padding: "4px 0",
-                        fontWeight: "bold",
-                        textTransform: "capitalize",
-                      }}
-                    >
-                      {cat.name}
+              {categories.map((cat) => (
+                <div
+                  key={cat._id}
+                  className="col-3"
+                  style={{ flex: "0 0 20%" }}
+                >
+                  <NavLink
+                    to={`/categories/${cat._id}`}
+                    className="text-decoration-none"
+                  >
+                    <div style={{ position: "relative" }}>
+                      <img
+                        src={cat.image}
+                        alt={cat.name}
+                        style={{
+                          width: "100%",
+                          height: "250px",
+                          objectFit: "cover",
+                          borderRadius: "10px",
+                        }}
+                      />
+                      <div
+                        style={{
+                          position: "absolute",
+                          bottom: 0,
+                          width: "100%",
+                          background: "rgba(255,255,255,0.8)",
+                          textAlign: "center",
+                          padding: "4px 0",
+                          fontWeight: "bold",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        {cat.name}
+                      </div>
                     </div>
-                  </div>
-                </NavLink>
-                 
-              </div>))}
+                  </NavLink>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-          <div>
-            <NavLink to="/products" className= "text-decoration-none">
-            <p>Explore Products</p>
-            </NavLink>
-          </div>
+        <hr />
+        <br />
+        <div>
+          <NavLink to="/products" className="text-decoration-none">
+            <button type="button" className="btn btn-primary">Explore Products</button>
+          </NavLink>
+        </div>
       </div>
     </>
   );
